c7c46ac9bcd3f0a202dc025521c728debb8af9c5
linglingandy <zeusandninja@gmail.com>
2021-02-28 00:14:27 -0800

    small fixes and add images/pid

--- Part2-Cypress/cypress/integration/party_horn/custom-tests.js
+++ Part2-Cypress/cypress/integration/party_horn/custom-tests.js
@@ -39,4 +39,48 @@ describe('Party Horn Tests', () => {
             expect($el).to.have.attr('src', './assets/media/audio/party-horn.mp3');
         });
     });
+
+    it('test volume image changes for all sound levels', () => {
+        cy.get('#volume-number').clear().type('0');
+
+        cy.get('#volume-image').then(($el) => {
+            expect($el).to.have.attr('src', './assets/media/icons/volume-level-0.svg');
+        });
+
+        cy.get('#volume-number').clear().type('10');
+
+        cy.get('#volume-image').then(($el) => {
+            expect($el).to.have.attr('src', './assets/media/icons/volume-level-1.svg');
+        });
+
+        cy.get('#volume-number').clear().type('40');
+
+        cy.get('#volume-image').then(($el) => {
+            expect($el).to.have.attr('src', './assets/media/icons/volume-level-2.svg');
+        });
+
+        cy.get('#volume-number').clear().type('80');
+
+        cy.get('#volume-image').then(($el) => {
+            expect($el).to.have.attr('src', './assets/media/icons/volume-level-3.svg');
+        });
+    });
+
+    it('test honk button is disabled', () => {
+        cy.get('#volume-number').clear();
+
+        cy.get('#honk-btn').then(($el) => {
+            expect($el).to.have.prop('disabled', true);
+        });
+
+        cy.get('#volume-number').clear().type('lmao');
+        cy.get('#honk-btn').then(($el) => {
+            expect($el).to.have.prop('disabled', true);
+        });
+    });
+
+    it('test error on invalid input', () => {
+        cy.get('#volume-number').clear().type('-1');
+        cy.get('#volume-number').invoke('prop', 'validity').should('include', { valid: false });
+    });
 });
